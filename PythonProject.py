b3c9070def85ad5f8030b5455e983e80a0a6c33a
Lu Chengwei <chengwei-lu@outlook.com>
2017-02-26 08:49:40 +0800

    Second version of part 3

--- PythonProject.py
+++ PythonProject.py
@@ -2,7 +2,19 @@
 # The group number is Lu Chengwei and Li Mingjun
 # The whole project is divided by four parts
 
-##################    Part I : Short and longterm bonds    ###############
+#import mudules which are used in our preject
+import pandas as pd
+import os
+import datetime
+import matplotlib.pyplot as plt
+import glob
+import math
+import random
+from random import randint
+import numpy as np
+###############################################################################################
+############################    Part I : Short and longterm bonds    ##########################
+###############################################################################################
 #Firstly, create the bond as a class
 class Bond(object):
     def __init__(self, term, amount, miniprice, miniterm, R_year):
@@ -14,53 +26,8 @@ class Bond(object):
     def GetComRate(self, t):
         self.R_com = (1 + self.R_year)**t - 1
         return self.R_com
-# class ShortBond(Bond):
-#     def __init__(self, term, amount, miniprice, miniterm, R_year):
-#         if 2 <= term < 5:
-#             self.term = term
-#         else:
-#             raise ValueError('The term of short bond is from 2 years to 5 years (not included)')
-#         if amount > 1000:
-#             self.amount = amount
-#         else:
-#             raise ValueError('The minimum amount of short bond is $1000')
-#         self.miniprice = miniprice
-#         self.miniterm = miniterm
-#         self.R_year = R_year
-#         self.miniprice = 1000
-#         self.miniterm = 2
-#         self.R_year = 0.01
-#
-#     def GetComRateS(self, t):
-#         # super is up to the class before which is a object "Bond"
-#         return super(ShortBond, self).GetComRate(t)
 
-# class LongBond(Bond):
-#     def __init__(self, term, amount, miniprice, miniterm, R_year):
-#         if term >= 5:
-#             self.term = term
-#         else:
-#             raise ValueError('The term of long bond is more than 5 years (included)')
-#         if amount > 3000:
-#             self.amount = amount
-#         else:
-#             raise ValueError('The minimum amount of short bond is $3000')
-#         self.miniprice = miniprice
-#         self.miniterm = miniterm
-#         self.R_year = R_year
-#         self.miniprice = 3000
-#         self.miniterm = 5
-#         self.R_year = 0.03
-#     def GetComRateL(self, t):
-#         return super(LongBond, self).GetComRate(t)
-
-
-
-
-#import the pakage for plotting
-import matplotlib.pyplot as plt
-
-#write function "mindollar" to abstract the change of minimumal price with time
+# write function "mindollar" to abstract the change of minimumal price with time
 def mindollar(iniprice,t,r):
     x = iniprice*(1+r)**t
     return x
@@ -74,5 +41,226 @@ plt.plot(x,y1)
 plt.plot(x,y2)
 plt.title('minimal price for different type of bonds')
 plt.legend(['short term bond', 'long term bond'])
+plt.show()
+
+###################################################################################
+##############################     part II : Stocks    ############################
+###################################################################################
+
+#IMPORT DATA
+datapath = 'H:\\UT1\M2\S2\python for finance\project\Data'
+
+list_df = list()
+
+for files in glob.glob(os.path.join(datapath,"*.csv")):
+        df = pd.read_csv(files,";")[[0,2]]
+        df['Date'] = df['Date'].astype('datetime64[ns]')
+        list_df.append(df)
+
+list_stock_name = ["AAPL", "AXP", "FDX", "GOOGLE", "IBM", "KO", "MS", "NOK", "XOM", "YHOO"]
+
+#Define the class 'Stock'
+class Stock(object):
+    def __init__(self, term, amount, name, number, date, price):
+        self.term = term
+        self.amount = amount
+        self.name = name
+        self.number = number
+        self.date = date
+        self.price = price
+#define a funtion to calculate the stock return
+def Price_Return_stock(startdate,enddate,name):
+    a = datetime.datetime.strptime(startdate, '%Y-%m-%d')
+    b = datetime.datetime.strptime(enddate, '%Y-%m-%d')
+    for (index, name_stock) in enumerate(list_stock_name):
+        if name_stock == name:
+            name_df = list_df[index]
+            break
+        else:
+            pass
+
+    while a not in list(name_df['Date']):
+        a = a - datetime.timedelta(days=1)
+    while b not in list(name_df['Date']):
+        b = b - datetime.timedelta(days=1)
+
+    Price = round(float(name_df[name_df['Date']== a]['High']),2)
+    Return = round(((float(name_df[name_df['Date']== b]['High']) - Price)/Price),2)
+    return Price, Return
+
+#plot prices of 10 stocks
+for (index,df) in enumerate(list_df):
+    plt.plot(df[[0]],df[[1]],label=list_stock_name[index])
+
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.title('Prices of 10 stocks')
+plt.legend()
+plt.show()
+###############################################################################################
+#################################      Part III : Investors        ############################
+###############################################################################################
+class Investor(object):
+    def __init__(self, Budget, mode, start):
+        self.Budget = Budget
+        self.mode = mode
+        self.start = start
+
+class D_Investor(Investor):
+    def __init__(self, Budget,start):
+        self.Budget = Budget
+        self.start = start
+        self.mode = 'Defensive'
+
+class A_Investor(Investor):
+    def __init__(self, Budget,start):
+        self.Budget= Budget
+        self.start = start
+        self.mode = 'Aggressive'
+
+class M_Investor(Investor):
+    def __init__(self, Budget,  start):
+        self.Budget = Budget
+        self.start = start
+        self.mode = 'Mixed'
+
+# short term bond and long term bond with the same term : 10 years,initial budget = 12000
+budget = 12000
+
+#this function is to calculate the return of every investor depending on different types and terms
+def Invest(investor,end_investment):
+    enddate = datetime.datetime.strptime(end_investment, '%Y-%m-%d')
+    startdate = datetime.datetime.strptime(investor.start, '%Y-%m-%d')
+    # while enddate not in list(list_df[0]['Date']):
+    #     enddate -= datetime.timedelta(days=1)
+    # while startdate not in list(list_df[0]['Date']):
+    #     startdate -= datetime.timedelta(days=1)
+
+    if investor.mode == 'Defensive':
+        if randint(0, 1) == 0:                 # the investor will invest only in the short bond
+            nb_SB = math.floor(investor.Budget / 1000)
+            nb_LB = 0
+
+        else:
+            nb_LB = math.floor(investor.Budget / 3000)
+            budget_LB = nb_LB * 3000
+            nb_SB = math.floor((investor.Budget - budget_LB) / 1000)
+
+        return_defen = round(3000 * nb_LB * (1 + 0.03) ** math.floor((enddate - startdate).days/365) + 1000 * nb_SB * (1 + 0.01) ** math.floor((enddate - startdate).days/365), 2)
+
+        return return_defen
+
+    elif investor.mode == 'Aggressive':
+
+        budgetleft = investor.Budget
+        list_nb = [0]*10
+
+        while budgetleft >= 100:
+
+            pick_stock = random.choice(list_stock_name)
+
+            for (index, name_stock) in enumerate(list_stock_name):
+                if name_stock == pick_stock:
+                    pick_df = list_df[index]
+
+                    price_stock = round(float(pick_df[pick_df['Date'] == startdate]['High']), 2)
+                    nb_stock = randint(0, math.floor(math.floor(budgetleft / price_stock) * 0.5))
+
+                    budgetleft -= price_stock * nb_stock
+                    list_nb[index] = list_nb[index] + nb_stock
+                    break
+                else:
+                    pass
+
+        return_stock = 0
+        for i, nb_stock in enumerate(list_nb):
+
+            price = round(float(list_df[i][list_df[i]['Date'] == enddate]['High']), 2)
+            return_stock += nb_stock * price
+
+        return_aggre= round(return_stock, 2)
+
+        return return_aggre
+
+    else:
+        if randint(0, 1) == 0:             # the situation that the investor chooses the bond
+            if randint(0, 1) == 0:         # the investor will invest only in the short bond
+                nb_SB = math.floor(investor.Budget / 1000)
+                nb_LB = 0
+
+            else:
+                nb_LB = math.floor(investor.Budget / 3000)
+                budget_LB = nb_LB * 3000
+                nb_SB = math.floor((investor.Budget - budget_LB) / 1000)
+
+            return_mix = round(3000 * nb_LB * (1 + 0.03) ** math.floor((enddate - startdate).days / 365) + 1000 * nb_SB * (
+                                1 + 0.01) ** math.floor((enddate - startdate).days / 365), 2)
+
+            return return_mix
+
+        else:  # this situation that the investor chooses the stock
+            budgetleft = investor.Budget
+            list_nb = [0] * 10
+
+            while budgetleft >= 1000:
+
+                pick_stock = random.choice(list_stock_name)
+
+                for (index, name_stock) in enumerate(list_stock_name):
+                    if name_stock == pick_stock:
+                        pick_df = list_df[index]
+
+                        Price_stock = round(float(pick_df[pick_df['Date'] == startdate]['High']), 2)
+                        nb_stock = randint(0, math.floor(math.floor(budgetleft / Price_stock) * 0.5))
+
+                        budgetleft -= Price_stock * nb_stock
+                        list_nb[index] = list_nb[index] + nb_stock
+                        break
+                    else:
+                        pass
+
+            return_stock = 0
+            for i, nb_stock in enumerate(list_nb):
+                price = round(float(list_df[i][list_df[i]['Date'] == enddate]['High']), 2)
+                return_stock += nb_stock * price
+
+                return_mix = round(return_stock, 2)
+
+            return return_mix
+
+# using the previous function 1000 times for each type of investors and calculate the average return in the end of the term
+Return_defen = [0]*1000
+for i in range(1000):
+    defen_investor = D_Investor(12000, "2005-01-03")
+    Return_defen[i] = Invest(defen_investor,"2015-01-05")
+Average_defen = math.floor(sum(Return_defen)/1000)
+
+Return_aggre = [0]*1000
+for i in range(1000):
+    aggr_investor = A_Investor(12000,"2005-01-03")
+    Return_aggre[i] = Invest(aggr_investor,"2015-01-05")
+Average_aggre = math.floor(sum(Return_aggre)/1000)
+
+Return_mix = [0]*1000
+for i in range(1000):
+    mix_investor = M_Investor(12000,"2005-01-03")
+    Return_mix[i] = Invest(mix_investor,"2015-01-05")
+Average_mix = math.floor(sum(Return_mix)/1000)
+
+print(Average_defen,Average_aggre,Average_mix)
+
+#plot the average return for different types of investors
+#define a function to put the exact number above every bar in our graph
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x()+rect.get_width()/2., 1.03*height, "%s" % float(height))
+
+plt.xlabel('types of investors')
+plt.ylabel('return in the end of term')
+plt.xticks((1,4,7),('Defensive','Aggressive','mixed'))
+rect = plt.bar(left=(1,4,7),height=(Average_defen,Average_aggre,Average_mix),width=0.60,align='center',yerr=0.0000000001,edgecolor = 'white')
+autolabel(rect)
+plt.title('average return for investors after 10 years')
 plt.show()
 
